5a01459f4a33322e7afda4b322434e55e0a13c1f
ambatitejaswini23 <ambatitejaswini82@gmail.com>
2026-06-09 16:04:49 +0530

    Frontend API fixed

--- app.py
+++ app.py
@@ -1,39 +1,42 @@
-# ADD_EXPENSE section
+if opt == "ADD_EXPENSE":
+
+
+st.header("ADD EXPENSE")
 
 with st.form("adding"):
 
-title = st.text_input("Expense Title")
+    title = st.text_input("Expense Title")
 
-amount = st.number_input("Amount", step=1)
+    amount = st.number_input("Amount", step=1)
 
-category = st.selectbox(
-    "Category", ["Food", "Travel", "Shopping", "Bills", "Entertainment"]
-)
-
-expense_date = st.date_input("Expense Date")
-
-btn = st.form_submit_button("ADD EXPENSE")
-
-if btn:
-
-    new_data = {
-        "title": title,
-        "amount": amount,
-        "category": category,
-        "expense_date": str(expense_date)
-    }
-
-    res = rq.post(
-        f"{server_loc}/add_expense",
-        json=new_data
+    category = st.selectbox(
+        "Category",
+        ["Food", "Travel", "Shopping", "Bills", "Entertainment"]
     )
 
-    st.success(res.json()["message"])
+    expense_date = st.date_input("Expense Date")
 
+    btn = st.form_submit_button("ADD EXPENSE")
 
-# VIEW_EXPENSES section
+    if btn:
+
+        new_data = {
+            "title": title,
+            "amount": amount,
+            "category": category,
+            "expense_date": str(expense_date)
+        }
+
+        res = rq.post(
+            f"{server_loc}/add_expense",
+            json=new_data
+        )
+
+        st.success(res.json()["message"])
+
 
 elif opt == "VIEW_EXPENSES":
+
 
 st.header("VIEW EXPENSES")
 
@@ -45,8 +48,6 @@ df = pd.DataFrame(data)
 
 st.dataframe(df)
 
-
-# DELETE_EXPENSE section
 
 elif opt == "DELETE_EXPENSE":
 
@@ -66,8 +67,6 @@ if st.button("DELETE"):
 
     st.success(res.json()["message"])
 
-
-# UPDATE_EXPENSE section
 
 elif opt == "UPDATE_EXPENSE":
 
