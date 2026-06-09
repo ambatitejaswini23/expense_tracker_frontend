633ea2ad8e0d2f1ea83cb2ee5f16ceecf16b818e
ambatitejaswini23 <ambatitejaswini82@gmail.com>
2026-06-09 16:00:13 +0530

    Frontend API fixed

--- app.py
+++ app.py
@@ -1,148 +1,113 @@
-import streamlit as st
-import requests as rq
-import pandas as pd
+# ADD_EXPENSE section
 
-server_loc = st.secrets["server_url"]
+with st.form("adding"):
 
+```
+title = st.text_input("Expense Title")
 
+amount = st.number_input("Amount", step=1)
 
-st.title("EXPENSE TRACKER APPLICATION")
+category = st.selectbox(
+    "Category", ["Food", "Travel", "Shopping", "Bills", "Entertainment"]
+)
 
+expense_date = st.date_input("Expense Date")
 
+btn = st.form_submit_button("ADD EXPENSE")
 
-opt = st.sidebar.selectbox(
-    "Choose Operation",["ADD_EXPENSE","VIEW_EXPENSES","DELETE_EXPENSE","UPDATE_EXPENSE","SEARCH_EXPENSE","SORT_EXPENSES"])
+if btn:
 
+    new_data = {
+        "title": title,
+        "amount": amount,
+        "category": category,
+        "expense_date": str(expense_date)
+    }
 
-if opt == "ADD_EXPENSE":
+    res = rq.post(
+        f"{server_loc}/add_expense",
+        json=new_data
+    )
 
-    st.header("ADD EXPENSE")
+    st.success(res.json()["message"])
+```
 
-    with st.form("adding"):
-
-        title = st.text_input("Expense Title")
-
-        amount = st.number_input("Amount", step=1)
-
-        category = st.selectbox(
-            "Category",[ "Food", "Travel","Shopping","Bills","Entertainment"]
-        )
-
-        btn = st.form_submit_button("ADD EXPENSE")
-
-        if btn:
-
-            new_data = {
-                "t": title,
-                "a": amount,
-                "c": category
-            }
-
-            res = rq.post(
-                f"{server_loc}/add_expense",
-                json=new_data
-            )
-
-            st.success(res.json()["msg"])
-
+# VIEW_EXPENSES section
 
 elif opt == "VIEW_EXPENSES":
 
-    st.header("VIEW EXPENSES")
+```
+st.header("VIEW EXPENSES")
 
-    res = rq.get(f"{server_loc}/view_expenses")
+res = rq.get(f"{server_loc}/get_expenses")
 
-    data = res.json()
+data = res.json()["expenses"]
 
-    df = pd.DataFrame(data)
+df = pd.DataFrame(data)
 
-    st.dataframe(df)
+st.dataframe(df)
+```
+
+# DELETE_EXPENSE section
 
 elif opt == "DELETE_EXPENSE":
-    st.header("DELETE EXPENSE")
-    expense_id=st.number_input("Enter Expense id",step=1)
-    if st.button("DELETE"):
-        res=rq.delete(f"{server_loc}/delete_expense/{expense_id}")
-        st.success(res.json()["msg"])
+
+```
+st.header("DELETE EXPENSE")
+
+expense_id = st.number_input(
+    "Enter Expense ID",
+    step=1
+)
+
+if st.button("DELETE"):
+
+    res = rq.delete(
+        f"{server_loc}/delete_expense/{expense_id}"
+    )
+
+    st.success(res.json()["message"])
+```
+
+# UPDATE_EXPENSE section
 
 elif opt == "UPDATE_EXPENSE":
 
-    st.header("UPDATE EXPENSE")
+```
+st.header("UPDATE EXPENSE")
 
-    expense_id = st.number_input(
-        "Enter Expense ID",
-        step=1
+expense_id = st.number_input(
+    "Enter Expense ID",
+    step=1
+)
+
+title = st.text_input("New Title")
+
+amount = st.number_input(
+    "New Amount",
+    step=1
+)
+
+category = st.selectbox(
+    "New Category",
+    ["Food", "Travel", "Shopping", "Bills", "Entertainment"]
+)
+
+expense_date = st.date_input("New Expense Date")
+
+if st.button("UPDATE"):
+
+    update_data = {
+        "title": title,
+        "amount": amount,
+        "category": category,
+        "expense_date": str(expense_date)
+    }
+
+    res = rq.put(
+        f"{server_loc}/update_expense/{expense_id}",
+        json=update_data
     )
 
-    title = st.text_input("New Title")
-
-    amount = st.number_input(
-        "New Amount",
-        step=1
-    )
-
-    category = st.selectbox("New Category",["Food","Travel","Shopping", "Bills","Entertainment"])
-
-    if st.button("UPDATE"):
-
-        update_data = {
-            "t": title,
-            "a": amount,
-            "c": category
-        }
-
-        res = rq.put(
-            f"{server_loc}/update_expense/{expense_id}",
-            json=update_data
-        )
-
-        st.success(res.json()["msg"])
-elif opt == "SEARCH_EXPENSE":
-
-    st.header("SEARCH EXPENSE")
-
-    category = st.selectbox(
-        "Choose Category",
-        [
-            "Food",
-            "Travel",
-            "Shopping",
-            "Bills",
-            "Entertainment"
-        ]
-    )
-
-    if st.button("SEARCH"):
-
-        res = rq.get(
-            f"{server_loc}/search_expense/{category}"
-        )
-
-        data = res.json()
-
-        df = pd.DataFrame(data)
-
-        st.dataframe(df)
-elif opt == "SORT_EXPENSES":
-
-    st.header("SORT EXPENSES")
-
-    sort_type = st.selectbox(
-        "Sort By",
-        [
-            "high",
-            "low"
-        ]
-    )
-
-    if st.button("SORT"):
-
-        res = rq.get(
-            f"{server_loc}/sort_expense/{sort_type}"
-        )
-
-        data = res.json()
-
-        df = pd.DataFrame(data)
-
-        st.dataframe(df)
+    st.success(res.json()["message"])
+```
